732c48c470e9ef12eda8c6583309409004c36c73
cbjoerke <93517188+cbjoerke@users.noreply.github.com>
2024-03-25 16:00:53 +0100

    Initial commit

---
+++ helpers/dicts.py
@@ -0,0 +1,54 @@
+traffic_light = {
+    "01" : {
+        "name" : "Svenskegrensen til Jæren",
+        "light" : "green"
+        },
+    "02" : {
+        "name" : "Ryfylket",
+        "light" : "yellow"
+        },
+    "03" : {
+        "name" :" Karmøy til Sotra",
+        "light" : "red"
+        },
+    "04" : {
+        "name" : "Nordhordland til Stadt",
+        "light" : "red"
+        },
+    "05" : {
+        "name" : "Stadt til Hustadvika",
+        "light" : "yellow"
+        },
+    "06" : {
+        "name" : "Nordmøre til Sør-Trøndelag",
+        "light" : "green"
+        },
+    "07" : {
+        "name" : "Nord-Trøndelag med Bindal", 
+        "light" : "yellow"
+        },
+    "08" : {
+        "name" : "Helgeland til Bodø",
+        "light" : "green"
+        },
+    "09" : {
+        "name" : "Vestfjorden og Vesterålen",
+        "light" : "green"
+        },
+    "10" : {
+        "name" : "Andøya til Senja",
+        "light" : "green"
+        },
+    "11" : {
+        "name" : "Kvaløy til Loppa",
+        "light" : "green"
+        },
+    "12" : {
+        "name" : "Vest-Finnmark",
+        "light" : "green"
+        },
+    "13" : {
+        "name" : "Øst-Finnmark",
+        "light" : "green"
+        }
+}
